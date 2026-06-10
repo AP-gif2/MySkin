5616a33872731fd4363735c56f149e928f8dad5e
AP-gif2 <Annelie.Peplow@hochschule-stralsund.de>
2026-06-10 21:35:41 +0200

    Add MainActivity.java

---
+++ app/src/main/java/org/hochschule_stralsund/myskin/MainActivity.java
@@ -0,0 +1,34 @@
+package org.hochschule_stralsund.myskin;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.BottomNavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import org.hochschule_stralsund.myskin.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+            BottomNavigationView navView = binding.bottomNavigation;
+            BottomNavigationUI.setupWithNavController(navView, navController);
+        }
+    }
+}
